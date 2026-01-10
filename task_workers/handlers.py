751c7f1d88ed04e2021c812418a35a32657a567d
Zach Mogambi <zachariahmogambi1@gmail.com>
2026-01-10 15:06:16 +0300

    updating handlers to specifically webhooks

--- task_workers/handlers.py
+++ task_workers/handlers.py
@@ -1,9 +1,66 @@
 import time
-import random
 import requests
 
+# def handle_email(payload):
+#     """
+#     simulating an email being sent (time.sleep(2) to simulate a network call) and returning
+#     a success result with message_id
+#     1. Print "Sending email to {to}..."
+#     2. Sleep for 2 seconds (simulate network call)
+#     3. Return success result with message_id
+#     """
+#     print(f"sending email to {payload['to']}...")
 
-JOB_HANDLERS = {}
+#     time.sleep(2)
+
+#     message_id = f"msg_{random.randint(100000, 999999)}"
+#     return {                                                                                                                                                
+#         "status": "sent",                                                                                                                                   
+#         "message_id": message_id,                                                                                                                           
+#         "to": payload['to']                                                                                                                                 
+#     }
+
+
+# def handle_image_processing(payload):
+#     """
+#     simulating an image processing by printing and time.sleep(3) then returning
+#     result with output url
+#     """
+
+#     print(f"Processing image {payload['image_url']}...")
+
+#     time.sleep(3)
+
+#     return {
+#         "status": "processed",
+#         "output_url": "https://cdn.example.com/processed_abc123.jpg",
+#         "operations_applied": payload['operations']
+#     }
+    
+
+# def handle_calculation(payload):
+#     """
+#     given a list of numbers, calculate sum, average, min, max of numbers and return results
+#     {
+#         "numbers": [1, 2, 3, 4, 5]
+#     }
+#     """
+#     numbers= payload.get("numbers", [])
+
+#     if not numbers:
+#         return {
+#             "sum": 0,
+#             "average": 0,
+#             "min": None,
+#             "max": None
+#         }
+
+#     return {
+#         "sum": sum(numbers),
+#         "average": sum(numbers) / len(numbers),
+#         "min": min(numbers),
+#         "max": max(numbers)
+#     }
 
 def process_job(job):
     """
@@ -56,6 +113,13 @@ def handle_webhook(payload):
         # This will catch connection errors, timeouts, and bad status codes,
         # triggering the fail_job logic in the worker.
         raise Exception(f"Webhook call failed: {e}")
+    
+JOB_HANDLERS = {
+    # "email": handle_email,
+    # "image_processing": handle_image_processing,
+    # "calculation": handle_calculation
+    "webhooks": handle_webhook
+}
 
 
 def handle_flaky_service(payload):
